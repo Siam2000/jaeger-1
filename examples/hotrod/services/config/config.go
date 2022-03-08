52905415083a3d22096d6f435c7c7add54866463
Siam2000 <101091374+Siam2000@users.noreply.github.com>
2022-03-07 23:37:27 -0600

    Update config.go

--- examples/hotrod/services/config/config.go
+++ examples/hotrod/services/config/config.go
@@ -24,13 +24,13 @@ var (
 
 	// RouteWorkerPoolSize is the size of the worker pool used to query `route` service.
 	// Can be overwritten from command line.
-	RouteWorkerPoolSize = 3
+	RouteWorkerPoolSize = 300
 
 	// 'customer' service
 
 	// MySQLGetDelay is how long retrieving a customer record takes.
 	// Using large value mostly because I cannot click the button fast enough to cause a queue.
-	MySQLGetDelay = 300 * time.Millisecond
+	MySQLGetDelay = 10 * time.Millisecond
 
 	// MySQLGetDelayStdDev is standard deviation
 	MySQLGetDelayStdDev = MySQLGetDelay / 10
